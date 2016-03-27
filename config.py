5bc20a7d91e96a980eaba064d21c1d9b4f32bce1
pika <tavukizartma@gmail.com>
2016-03-27 22:22:15 +0800

    ignore file

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-DEBUG = True
-SECRET_KEY = "ld5rcpr6d7"
-MAILGUN_KEY = "key-15f83f4a591c0a6e9f067550a967391b"
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'guestbook.db')
