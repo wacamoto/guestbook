7730fc3b31e8ebeb219e17e4cea6f41ea9288433
pika <pika@ip143-103.wlan.ntust.edu.tw>
2016-03-27 16:49:37 +0800

    remove sendmail.py text

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 DEBUG = True
 SECRET_KEY = "ld5rcpr6d7"
+MAILGUN_KEY = "key-15f83f4a591c0a6e9f067550a967391b"
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_TRACK_MODIFICATIONS = True
--- guestbook/sendmail.py
+++ guestbook/sendmail.py
@@ -1,9 +1,11 @@
 import requests
+from config import MAILGUN_KEY as mailgunKey
+
 
 def Verificationletter(usermail, token):
     return requests.post(
         "https://api.mailgun.net/v3/sandbox9715dcdec1e043b3ab37d4ccf5f7b3c4.mailgun.org/messages",
-        auth=("api", "key-15f83f4a591c0a6e9f067550a967391b"),
+        auth=("api", mailgunKey),
         data={
             "from": "<waka@sandbox9715dcdec1e043b3ab37d4ccf5f7b3c4.mailgun.org>",
             "to": [usermail, "<waka@sandbox9715dcdec1e043b3ab37d4ccf5f7b3c4.mailgun.org>"],
@@ -22,3 +24,4 @@ def Verificationletter(usermail, token):
         }
     )
 
+Verificationletter('tavukizartma@gmail.com', 'tokenishere')
